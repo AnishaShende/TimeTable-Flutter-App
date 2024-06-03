83a241a92978f05b99a8e04d14eb8e3776b9d918
Anisha Shende <91362696+AnishaShende@users.noreply.github.com>
2024-06-03 23:08:21 +0530

    second commit

--- timetable_app/lib/component/lecture_box.dart
+++ timetable_app/lib/component/lecture_box.dart
@@ -1,223 +1,163 @@
 import 'package:flutter/material.dart';
 
 class LectureBox extends StatelessWidget {
-  const LectureBox({super.key});
+  const LectureBox(
+      {super.key,
+      required this.height,
+      required this.width,
+      required this.lectureStatus,
+      required this.startTime,
+      required this.endTime,
+      required this.subject,
+      required this.teacherInitials,
+      required this.classroomNumber});
+
+  final double height;
+  final double width;
+  final String lectureStatus;
+  final String startTime;
+  final String endTime;
+  final String subject;
+  final String teacherInitials;
+  final String classroomNumber;
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      left: 21,
-      top: 450,
-      child: SizedBox(
-        width: 347,
-        height: 153,
-        child: Stack(
-          children: [
-            Positioned(
-              left: 0,
-              top: 0,
-              child: Container(
-                width: 347,
-                height: 153,
+    var color = lectureStatus == "PREVIOUS LECTURE"
+        ? Colors.blue //const Color(0xFFFF5A5A)
+        : lectureStatus == "CURRENT LECTURE"
+            ? Colors.red
+            : Colors.green;
+    var fontSize = lectureStatus == "PREVIOUS LECTURE"
+        ? 15.0
+        : lectureStatus == "CURRENT LECTURE"
+            ? 20.0
+            : 15.0;
+    return Container(
+      height: height,
+      width: width,
+      decoration: ShapeDecoration(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          side: const BorderSide(width: 1, color: Color(0xFF7CDEFF)),
+          borderRadius: BorderRadius.circular(19),
+        ),
+      ),
+      // width: 347,
+      // height: 153,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.0001,
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 4),
+            // width: 273,
+            // height: 34,
+            decoration: ShapeDecoration(
+              color: const Color(0xFFF0F0F0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(19),
+              ),
+            ),
+            child: Text(
+              lectureStatus,
+              style: TextStyle(
+                color: color, // Color(0xFFFF5A5A),
+                fontSize: fontSize,
+                fontFamily: 'Sofia Sans',
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "$subject ",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: fontSize + 5,
+                  fontFamily: 'Sofia Sans',
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Text(
+                "($teacherInitials)",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: fontSize + 5,
+                  fontFamily: 'Sofia Sans',
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(5),
+                // width: 67,
+                // height: 24,
                 decoration: ShapeDecoration(
-                  color: Colors.white,
+                  color: const Color(0xFFF0F0F0),
                   shape: RoundedRectangleBorder(
-                    side: const BorderSide(width: 1, color: Color(0xFF7CDEFF)),
                     borderRadius: BorderRadius.circular(19),
                   ),
                 ),
-              ),
-            ),
-            Positioned(
-              left: 37,
-              top: 8,
-              child: SizedBox(
-                width: 273,
-                height: 34,
-                child: Stack(
-                  children: [
-                    Positioned(
-                      left: 0,
-                      top: 0,
-                      child: Container(
-                        width: 273,
-                        height: 34,
-                        decoration: ShapeDecoration(
-                          color: const Color(0xFFF0F0F0),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(19),
-                          ),
-                        ),
-                      ),
-                    ),
-                    const Positioned(
-                      left: 52,
-                      top: 5,
-                      child: Text(
-                        'CURRENT LECTURE',
-                        style: TextStyle(
-                          color: Color(0xFFFF5A5A),
-                          fontSize: 20,
-                          fontFamily: 'Sofia Sans',
-                          fontWeight: FontWeight.w600,
-                          height: 0,
-                        ),
-                      ),
-                    ),
-                  ],
+                child: Text(
+                  startTime, // '9:00 AM ',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: fontSize - 5,
+                    fontFamily: 'Sofia Sans',
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
               ),
-            ),
-            Positioned(
-              left: 27,
-              top: 112,
-              child: SizedBox(
-                width: 67,
-                height: 24,
-                child: Stack(
-                  children: [
-                    Positioned(
-                      left: 0,
-                      top: 0,
-                      child: Container(
-                        width: 67,
-                        height: 24,
-                        decoration: ShapeDecoration(
-                          color: const Color(0xFFF0F0F0),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(19),
-                          ),
-                        ),
-                      ),
-                    ),
-                    const Positioned(
-                      left: 12,
-                      top: 5,
-                      child: Text(
-                        '9:00 AM ',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 12,
-                          fontFamily: 'Sofia Sans',
-                          fontWeight: FontWeight.w700,
-                          height: 0,
-                        ),
-                      ),
-                    ),
-                  ],
+              Container(
+                padding: const EdgeInsets.all(5),
+                decoration: ShapeDecoration(
+                  color: const Color(0xFFF0F0F0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(19),
+                  ),
+                ),
+                child: Text(
+                  classroomNumber,
+                  style: TextStyle(
+                    color: Color(0xFF001E8B),
+                    fontSize: fontSize + 10,
+                    letterSpacing: 1.5,
+                    fontFamily: 'Sofia Sans',
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
               ),
-            ),
-            Positioned(
-              left: 254,
-              top: 112,
-              child: SizedBox(
-                width: 67,
-                height: 24,
-                child: Stack(
-                  children: [
-                    Positioned(
-                      left: 0,
-                      top: 0,
-                      child: Container(
-                        width: 67,
-                        height: 24,
-                        decoration: ShapeDecoration(
-                          color: const Color(0xFFF0F0F0),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(19),
-                          ),
-                        ),
-                      ),
-                    ),
-                    const Positioned(
-                      left: 12,
-                      top: 5,
-                      child: Text(
-                        '10:00 AM ',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 12,
-                          fontFamily: 'Sofia Sans',
-                          fontWeight: FontWeight.w700,
-                          height: 0,
-                        ),
-                      ),
-                    ),
-                  ],
+              Container(
+                padding: const EdgeInsets.all(5),
+                decoration: ShapeDecoration(
+                  color: const Color(0xFFF0F0F0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(19),
+                  ),
+                ),
+                child: Text(
+                  endTime,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: fontSize - 5,
+                    fontFamily: 'Sofia Sans',
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
               ),
-            ),
-            const Positioned(
-              left: 174,
-              top: 52,
-              child: Text(
-                '(VBM)',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 40,
-                  fontFamily: 'Sofia Sans',
-                  fontWeight: FontWeight.w200,
-                  height: 0,
-                ),
-              ),
-            ),
-            const Positioned(
-              left: 89,
-              top: 52,
-              child: Text(
-                'FDS',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 40,
-                  fontFamily: 'Sofia Sans',
-                  fontWeight: FontWeight.w700,
-                  height: 0,
-                ),
-              ),
-            ),
-            Positioned(
-              left: 114,
-              top: 100,
-              child: SizedBox(
-                width: 120,
-                height: 48,
-                child: Stack(
-                  children: [
-                    Positioned(
-                      left: 0,
-                      top: 2,
-                      child: Container(
-                        width: 120,
-                        height: 44,
-                        decoration: ShapeDecoration(
-                          color: const Color(0xFFF0F0F0),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(19),
-                          ),
-                        ),
-                      ),
-                    ),
-                    const Positioned(
-                      left: 16,
-                      top: 0,
-                      child: Text(
-                        'B301',
-                        style: TextStyle(
-                          color: Color(0xFF001E8B),
-                          fontSize: 40,
-                          fontFamily: 'Sofia Sans',
-                          fontWeight: FontWeight.w400,
-                          height: 0,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+            ],
+          ),
+        ],
       ),
     );
   }
